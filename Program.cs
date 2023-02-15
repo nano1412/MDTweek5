75fa6dddc2d4b8b050ddcdebd6f3c268681d62b9
Nano1412 <nano.khongsri@gmail.com>
2023-02-15 14:55:16 +0700

    car interest

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace costomname {
+     class Program {            
+          static void Main(string[] args) {
+          carinterest();
+          }
+          
+          static void carinterest(){
+               int car,down,rate,interest,total;
+
+               Console.Write("car price:");
+               car = int.Parse(Console.ReadLine());
+               Console.Write("front payment price:");
+               down = int.Parse(Console.ReadLine());
+               Console.Write("rate(%):");
+               rate = int.Parse(Console.ReadLine());
+
+               interest = ((car-down) * (rate/100) * 4);
+               total = (car-down) + interest;
+               Console.WriteLine("payment per month = {0} THB", total/12);
+          }
+     }
+}
