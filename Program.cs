6a30ad7b1eb7b12f77307b3014170bb2942125fb
Nano1412 <nano.khongsri@gmail.com>
2023-02-15 15:12:23 +0700

    ifelse and twopositive

--- Program.cs
+++ Program.cs
@@ -3,7 +3,9 @@
 namespace costomname {
      class Program {            
           static void Main(string[] args) {
-          carinterest();
+               //carinterest();
+               //ifelse();
+               twopositive();
           }
           
           static void carinterest(){
@@ -19,6 +21,20 @@ namespace costomname {
                interest = ((car-down) * (rate/100) * 4);
                total = (car-down) + interest;
                Console.WriteLine("payment per month = {0} THB", total/12);
+          }
+          static void ifelse(){
+               Console.Write("Number:");
+               int num = int.Parse(Console.ReadLine());
+               if (num > 0){Console.WriteLine("positive");}
+          }
+          static void twopositive(){
+               Console.Write("Number1:");
+               int num1 = int.Parse(Console.ReadLine());
+               Console.Write("Number2:");
+               int num2 = int.Parse(Console.ReadLine());
+               if(num1>0 && num2>0){
+                    Console.WriteLine("positive");
+               }
           }
      }
 }
